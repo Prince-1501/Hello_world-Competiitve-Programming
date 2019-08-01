27e6ab5f123d2cfee6d764595987d7a4dec83851
Prince-1501 <37762770+Prince-1501@users.noreply.github.com>
2019-08-01 22:49:29 +0530

    Create 46_drink_codeforce.cpp

---
+++ 46_drink_codeforce.cpp
@@ -0,0 +1,40 @@
+//
+//  main.cpp
+//  hello
+//
+//  Created by Prince  Kumar on 01/08/19.
+//  Copyright © 2019 Prince  Kumar. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n; cin>>n;
+    // p[i] =  how much ml of orange juice
+    int p[n]; for(int i=0;i<n;i++)
+    {
+        int x ;cin>>x;
+        x = x*10;
+        p[i] = x;
+    }
+    // we calculate total orange juice
+    
+    int tot_orange  = 0;
+    for(int i=0;i<n;i++)
+    {
+        tot_orange+=p[i];
+    }
+    
+    double x = tot_orange*100.00;
+    //  / we calculate total orange juice
+    
+    int tot_vol = n*1000; //integer
+    
+    // we calcuate total volume
+    
+    double res = (x/tot_vol);
+    cout<<res<<endl;
+    
+     
+}

b625df4b4c3d8558344c1a41865e7bdc5432b789
Prince-1501 <37762770+Prince-1501@users.noreply.github.com>
2020-05-08 23:47:48 +0530

    Create Array_in_data_structures.cpp

---
+++ Data Structures/Array_in_data_structures.cpp
@@ -0,0 +1,41 @@
+//
+//  DATA STRUCTURES.cpp
+//
+//  Created by Prince  Kumar on 08/05/2020.
+//  Copyright © 2020 Prince  Kumar. All rights reserved.
+//
+//                      ---** ARRAYS in C++ **---
+
+#include <iostream>
+using namespace std;
+int main(){
+    
+    //1st method
+//    int arr[5];
+//    arr[0] = 4;
+//    arr[1] = 3;
+//    arr[2] = -10;
+//    arr[3] = 8;
+//    arr[4] = -6;
+    
+    // 2nd method
+    int arr[5] = {4 , 3, -10, 8,-9};
+    
+    int arr1[] = {4 , 3, -10, 8,7 , 4, 2 , 11};
+    //int arr2[];
+    
+    
+    cout<<arr[3]<<endl;
+    cout<<arr1[5]<<endl;
+    cout<<"Ans : "<<arr1[3/2]<<endl;
+    
+    
+    int a[5]={0};
+    cout<<a[4]<<endl;
+    
+//    int b[5]={1};
+//    cout<<b[3]<<endl;
+}
+
+
+

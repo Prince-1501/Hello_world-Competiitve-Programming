8302a5d0f6aa3038047585aba09d748b3bf81702
Prince-1501 <37762770+Prince-1501@users.noreply.github.com>
2020-06-29 09:21:26 +0530

    Create Binary search in STL.cpp

---
+++ STL/Binary search in STL.cpp
@@ -0,0 +1,36 @@
+//  DATA STRUCTURES.cpp
+//
+//  Created by Prince  Kumar on 28/06/2020.
+//  Copyright © 2020 Prince  Kumar. All rights reserved.
+//
+//                 ---** STANDARD TEMPLATE LIBRARY (STL) in C++ **---
+//                         ---** PRACTICE CODING SKILLS **---
+//                  all_of | any_of | none_of | copy_n | iota
+
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+void show(int a[], int asize){
+    for(int i=0;i<asize;i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+}
+int main(){
+    int a[] = { 2, 3, 0 , 9, 5, 6, 8, 1, 4};
+    int size = sizeof(a)/sizeof(a[0]);
+    
+    show(a, size);
+    
+    cout<<"array after sorting"<<endl;
+    sort(a, a+size);//
+    show(a, size);
+    
+    if(binary_search(a, a+size, 18)){
+        cout<<"Yes our element is present in the array"<<endl;
+    }else{
+        cout<<"Our element is not present in the array"<<endl;
+    }
+}
+
